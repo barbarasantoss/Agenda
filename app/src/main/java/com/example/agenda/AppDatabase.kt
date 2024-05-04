942db0055dd76a8740c57ac0b7f21e53534fd030
barbarasantoss <santosbarbara716@gmail.com>
2024-05-04 19:53:09 -0300

    first commit

---
+++ app/src/main/java/com/example/agenda/AppDatabase.kt
@@ -0,0 +1,40 @@
+package com.example.agenda
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.agenda.dao.UsuarioDao
+import com.example.agenda.model.Usuario
+
+
+@Database(entities = [Usuario::class], version = 1 )
+
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun usuarioDao(): UsuarioDao
+
+    companion object{
+
+        //definindo o banco
+        private const val DATABASE_NAME: String = "DB_Usuario"
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstace(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+                INSTANCE =  instance
+                instance
+            }
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/agenda/model/Usuario.kt
@@ -0,0 +1,20 @@
+package com.example.agenda.model
+
+import androidx.room.Entity
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "tabela_usuarios")
+data class Usuario (
+
+    @ColumnInfo(name = "nome") val nome: String,
+    @ColumnInfo(name = "sobrenome") val sobrenome: String,
+    @ColumnInfo(name = "idade") val idade: String,
+    @ColumnInfo(name = "celular") val celular: String,
+    ){
+
+    @PrimaryKey(autoGenerate = true)
+    var vid: Int = 0
+
+}
